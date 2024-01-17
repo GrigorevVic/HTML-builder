449b0148799c4cb43e379f890bbb6e6212f7fefd
Grigorev Viktor <4342718@gmail.com>
2024-01-17 17:23:41 +0300

    feat: add 3rd task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const fs = require('fs/promises');
+const fss = require('fs');
+
+const fileName = "secret-folder";
+const getPath = (filename) => path.join(__dirname, filename);
+
+const pathToDir = getPath(fileName);
+
+const unionFiles = async (pathToDir) => {
+    const data = await fs.readdir(pathToDir, { withFileTypes: true });
+    const files = data.filter(file => file.isFile());
+    files.forEach(file => {
+        const pathToFile = getPath(path.join(fileName, file.name));
+        const ext = path.extname(pathToFile);
+        const name = path.basename(pathToFile, ext);
+        fss.stat(pathToFile, (error, stats) => {
+            console.log(`${name} - ${ext.slice(1)} - ${stats.size}b`);
+        });
+    });
+};
+
+unionFiles(pathToDir);
+
+
