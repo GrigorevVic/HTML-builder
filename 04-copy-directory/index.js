002c4dffe1fa6fbc078643fbe22b4eee22b1769b
Grigorev Viktor <4342718@gmail.com>
2024-01-25 19:59:31 +0300

    fix: delete dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const fss = require('fs/promises');
 const path = require('path');
 
@@ -9,25 +8,17 @@ const getPath = (filename) => path.join(__dirname, filename);
 const pathToDir = getPath(fileName);
 const pathToDirCopy = getPath(fileNameCopy);
 
-fs.mkdir(pathToDirCopy,
-    { recursive: true },
-    (err) => {
-        if (err) {
-            return console.error(err);
-        }
-        console.log('Directory created successfully!\n');
-    });
-
 const copyFiles = async () => {
+    await fss.rm(pathToDirCopy, { recursive: true, force: true });
+    await fss.mkdir(pathToDirCopy, { recursive: true });
     const data = await fss.readdir(pathToDir, { withFileTypes: true });
     data.forEach(async (file) => {
-        console.log(file)
         const pathToFile = getPath(path.join(fileName, file.name));
         const pathToFileCopy = getPath(path.join(fileNameCopy, file.name));
         try {
             await fss.copyFile(pathToFile, pathToFileCopy);
             console.log(`${file.name}  successfully copied from '${fileName}' to '${fileNameCopy}'`);
-        } catch (e){
+        } catch (e) {
             console.error('The file could not be copied\n', e);
         }
     });
